1955c3b505889cbfd19e4e66a4154bdde5739dab
Richard Goode <rgoode494@gmail.com>
2026-03-12 15:27:19 -0500

    UPDATE: Added pokemonAPI feature and updated css, html, and js respectively.

--- jscript/site.js
+++ jscript/site.js
@@ -1,4 +1,4 @@
-// Dynamic Welcome Message
+// Dynamic Welcome Message //
 const hours = new Date().getHours() // get the current hour
 
 const isMorning = hours >= 4 && hours < 12 // is it morning?
@@ -22,13 +22,13 @@ welcomeDiv.appendChild(welcomeH1)
 
 
 
-// Secret Message
+// Secret Message //
 const key = "It's a secret to everbody."
 localStorage.setItem(key, "HEY, LISTEN!")
 
 
 
-// Image Carousel
+// Image Carousel //
 const urls = [
     'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -71,3 +71,43 @@ btnPrev.addEventListener("click", () => {
     currentImage--
     showImages()
 })
+
+
+
+// PokeAPI Sprite Fetch //
+// pokeAPI fetch request
+const retrievePokemonData = async url => {
+    const respone = await fetch(url)
+    return await respone.json()
+}
+
+// fetch pokemon name and sprite
+const getRandomPokemon = async pokemon => {
+    // random pokemon selector
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 151)
+    // api data variable
+    const storedPokemon = await retrievePokemonData(url)
+    // destructured object
+    const { name, sprites } = storedPokemon
+    // return object
+    return {
+        name: storedPokemon.name,
+        sprite: storedPokemon.sprites.front_default
+    }
+}
+
+// render pokemon sprite and name.
+const renderPokemon = async () => {
+    // pokemon variable wiith random pokemon object
+    const pokemon = await getRandomPokemon()
+    // select elements
+    const pokeSprite = document.getElementById('pokemon_sprite')
+    const pokeName = document.getElementById('pokemon_name')
+    // fill elements
+    pokeSprite.src = pokemon.sprite
+    pokeName.textContent = `It's ${pokemon.name}!`.toLocaleUpperCase()
+}
+
+// display pokemon sprite and name to homepage
+renderPokemon()
+
